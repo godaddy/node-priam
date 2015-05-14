475f2db7ed51258228832a27d8038d29963924ff
Fady Matar <fmatar@godaddy.com>
2015-05-14 13:30:46 -0700

    Working on Gulp integration

--- test/unit/driver.tests.js
+++ test/unit/driver.tests.js
@@ -32,33 +32,9 @@ describe('lib/driver.js', function () {
       assert.instanceOf(driver, datastax.DatastaxDriver);
     });
 
-//    it('returns helenus driver if specified as "helenus" and null cqlVersion', function () {
-//      testInstance('helenus', 'cqlVersion', null, 'helenus', '3.1.0', 'thrift', helenus.HelenusDriver);
-//    });
-//
-//    it('returns helenus driver if specified as "thrift" and null cqlVersion', function () {
-//      testInstance('thrift', 'cqlVersion', null, 'helenus', '3.1.0', 'thrift', helenus.HelenusDriver);
-//    });
-//
-//    it('returns helenus driver if cqlVersion is less than 3', function () {
-//      testInstance('datastax', 'cqlVersion', '2.0.0', 'helenus', '2.0.0', 'thrift', helenus.HelenusDriver);
-//    });
-
     it('returns datastax driver if cqlVersion is equal to 3.0', function () {
       testInstance('datastax', 'cqlVersion', '3.0.0', 'datastax', '3.0.0', 'binary', datastax.DatastaxDriver);
     });
-
-//    it('returns helenus driver if specified as "helenus" and null version', function () {
-//      testInstance('helenus', 'version', null, 'helenus', '3.1.0', 'thrift', helenus.HelenusDriver);
-//    });
-//
-//    it('returns helenus driver if specified as "thrift" and null version', function () {
-//      testInstance('thrift', 'version', null, 'helenus', '3.1.0', 'thrift', helenus.HelenusDriver);
-//    });
-//
-//    it('returns helenus driver if version is less than 3', function () {
-//      testInstance('datastax', 'version', '2.0.0', 'helenus', '2.0.0', 'thrift', helenus.HelenusDriver);
-//    });
 
     it('returns datastax driver if version is greater than 3.0', function () {
       testInstance('datastax', 'version', '3.1.0', 'datastax', '3.1.0', 'binary', datastax.DatastaxDriver);
